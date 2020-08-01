5036d80f6cd629f8fed36cfe259dd45a60471a5b
eriksanchez <erikcsanchezdev@gmail.com>
2020-07-31 21:22:42 -0500

    Country list in progress...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "India"]);
+  useEffect(() => {
+    // asyn => send a request, wait for it, do something with it
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, //United States, United Kingdom
+            value: country.countryInfo.iso2, //UK, USA, FR
+          }));
+        });
+    };
+  }, []);
   return (
     <div className="app">
-      <h1>Lets build a COVID-19 tracker??</h1>
+      <div className="app_header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select varient="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+
+      {/*Title + Select input dropdown field */}
+
+      {/*InfoBoxes*/}
+      {/*InfoBoxes*/}
+      {/*InfoBoxes*/}
+
+      {/*Table*/}
+      {/*Graph*/}
+
+      {/*Map*/}
     </div>
   );
 }
